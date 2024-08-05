f918c4a242435e169017ad36ba6163da50f3c1f5
Kyu Min Shim <kmshim@uwaterloo.ca>
2024-08-05 15:41:54 -0400

    render dataframe

--- sales/app.py
+++ sales/app.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+import pandas as pd
+
 import plotly.express as px
-from shiny.express import input, ui
+from shiny import reactive
+from shiny.express import render, input, ui
 from shinywidgets import render_plotly
 
-ui.page_opts(title="Filling layout", fillable=True)
+ui.page_opts(title="Sales Dashboard - 1 of 5", fillable=True)
+
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "data/sales.csv"
+    return pd.read_csv(infile)
+
 with ui.layout_columns():
-    @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y='tip')
     
-    @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y='total_bill')
+    @render.data_frame
+    def data():
+        return dat()
